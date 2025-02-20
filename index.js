5d07b5367aa0bbe4a52d8cda05521a9ebef1c45f
icemedia001 <teniopemipo@gmail.com>
2025-02-20 13:44:12 +0100

    Project Setup

---
+++ index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
