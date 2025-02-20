f315e70feccb522084b8163e9271eeacd50ff317
icemedia001 <teniopemipo@gmail.com>
2025-02-20 14:03:14 +0100

    Add integrations json

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT
 
 app.use(bodyParser.json());
 
